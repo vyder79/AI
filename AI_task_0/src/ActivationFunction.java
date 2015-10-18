b8635e5b704e9b20a2cc8b64d087b0fe77c39559
vyder <vyder@lapek>
2015-10-18 19:25:11 +0200

    Uzupełnienie Neuronu, dodanie klasy Funkcji Aktywacji.

---
+++ AI_task_0/src/ActivationFunction.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ */
+
+/**
+ * @author vyder
+ *
+ */
+public class ActivationFunction {
+	
+	
+
+}
--- AI_task_0/src/Neuron.java
+++ AI_task_0/src/Neuron.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -7,5 +9,56 @@
  *
  */
 public class Neuron {
+	
+	ArrayList<Double> xList = new ArrayList<>();
+	ArrayList<Double> weights = new ArrayList<>();
+	ActivationFunction aFunction = new ActivationFunction();
+	double z = 0;
+	
+	public Neuron(){
+		this.xList = new ArrayList<>();
+		this.weights = new ArrayList<>();
+		ActivationFunction aFunction = new ActivationFunction();
+		this.z = 0;
+	}
+	
+	public Neuron(ArrayList x_list, ArrayList weights_list){
+		this.xList = x_list;
+		this.weights = weights_list;
+		
+	}
+
+	// getters and setters
+	public ArrayList<Double> getWeights() {
+		return weights;
+	}
+
+	public void setWeights(ArrayList<Double> weights) {
+		this.weights = weights;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
+	public ActivationFunction getaFunction() {
+		return aFunction;
+	}
+
+	public void setaFunction(ActivationFunction aFunction) {
+		this.aFunction = aFunction;
+	}
+
+	public ArrayList<Double> getxList() {
+		return xList;
+	}
+
+	public void setxList(ArrayList<Double> xList) {
+		this.xList = xList;
+	}
 
 }
