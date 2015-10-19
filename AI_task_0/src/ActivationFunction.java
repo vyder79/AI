dde8820c345fedb8f855158ecf181ee9ed8c0028
vyder <vyder@tomasz-lap-hc>
2015-10-19 14:43:46 +0200

    Uzupełnienie ActivationFunction o konstruktor.

--- AI_task_0/src/ActivationFunction.java
+++ AI_task_0/src/ActivationFunction.java
@@ -1,8 +1,14 @@
 /**
+ * At this moment this is a linear activation function
+ * 
  * @author vyder
  *
  */
 public class ActivationFunction {
+	
+	public ActivationFunction() {
+		// TODO Auto-generated constructor stub
+	}
 	
 	public double func(double value){
 		
