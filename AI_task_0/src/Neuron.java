3713edf1f1dc531e0ec67299cfa3a27ae43af7e3
vyder <vyder@lapek>
2015-10-21 00:12:29 +0200

    Porządki w kodzie klasy Neuron, dodanie komentarzy i javadoc'ów.

--- AI_task_0/src/Neuron.java
+++ AI_task_0/src/Neuron.java
@@ -2,14 +2,23 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
+ * Class Neuron to compute output for given input signals
+ * using list of weights for every input and activation function.
+ * 
  * @author vyder
  *
  */
 public class Neuron {
-	/**  */
+	
+	/** list of neuron weights */
 	ArrayList<Double> weights = new ArrayList<>();
+	
+	/** neuron's activation function */
 	ActivationFunction aFunction = new ActivationFunction();
 	
+	/** 
+	 * constructors 
+	 */
 	public Neuron(){
 		this.weights = new ArrayList<>();
 		this.aFunction = new ActivationFunction();
@@ -19,33 +28,30 @@ public class Neuron {
 		this.weights = weights_list;	
 	}
 	
-	// learning neuron
-	public double learn(ArrayList<Double> inputs, double output){
-		double z = 0;
-		for (int i = 0; i < inputs.size(); i++) {
-			z += inputs.get(i) * this.weights.get(i);
-		}
-
-		return aFunction.func(z);
-	}
-	
-	// learned neuron
+	/**
+	 *  learned neuron
+	 *  
+	 * @param inputs
+	 * @return
+	 */
 	public double activate(ArrayList<Double> inputs){
 		double z = 0;
 		for (int i = 0; i < inputs.size(); i++) {
 			z += inputs.get(i) * this.weights.get(i);
 		}
-
 		return aFunction.func(z);
 	}
 	
-	// set random weights at the start of learning
+	/**
+	 *  set random weights at the start of learning
+	 *  
+	 * @param tp - trening pattern
+	 */
 	public void setRandomWeights(TreningPattern tp) {
 		setRandomWeights(tp.getInputList(), tp.getOutput());
 	}
 	
 	private void setRandomWeights(ArrayList<Double> inputData, double output) {
-		
 		double weightsSum = 0d;
 		int count = inputData.size() - 1;
 		Random r = new Random();
@@ -61,32 +67,34 @@ public class Neuron {
 		this.weights.add((output - weightsSum) / inputData.get(count));
 	}
 	
-	// learning using backward error propagation
+	/**
+	 *  learning using backward error propagation
+	 *  
+	 * @param tp
+	 * @param epsilon
+	 * @return
+	 */
 	public double backwardErrorPropagation(TreningPattern tp, double epsilon) {
 		return backwardErrorPropagation(tp.getInputList(), tp.getOutput(), epsilon);
 	}
 	
 	private double backwardErrorPropagation(ArrayList<Double> inputs, double output, double epsilon) {
-
 		double errorValue = output - activate(inputs);
 		ArrayList<Double> newWeights = new ArrayList<>();
-
 		for (int i = 0; i < this.weights.size(); i++) {
 			newWeights.add(this.weights.get(i) + (epsilon * errorValue) * inputs.get(i));
 		}
-		
 		this.weights.clear();
 		this.weights = newWeights;
-		
 		if (Test.DEBUG) {
 			System.out.println("errorValue " + Math.abs(errorValue));
 		}
-		
 		return errorValue;
 	}
 
-	
-	// getters and setters
+	/**
+	 *  getters and setters
+	 */
 	public ArrayList<Double> getWeights() {
 		return weights;
 	}
--- AI_task_0/src/Test.java
+++ AI_task_0/src/Test.java
@@ -1,13 +1,23 @@
 import java.util.ArrayList;
 
+/**
+ * Test class to check Neuron in action.
+ * 
+ * @author vyder
+ *
+ */
 public class Test {
 	
+	// when TRUE, additional data is shown when computing weights
 	final static boolean DEBUG = false;
 
+	
 	public static void main(String[] args) {
 		
+		// create neuron
 		Neuron n = new Neuron();
 		
+		// testing data
 		ArrayList<Double> inputs = new ArrayList<>();
 		inputs.add(0.10001);
 		inputs.add(0.36754);
@@ -22,8 +32,10 @@ public class Test {
 		inputs2.add(0.002009);
 		TreningPattern tp2 = new TreningPattern(inputs2, 0.1411111);
 		
+		// set random weights
 		n.setRandomWeights(tp);
 		
+		// show start data
 		System.out.println("initial weights: "+ n.getWeights().toString());
 		System.out.println("inputs for 1 data: " + inputs.toString());
 		System.out.println("output for 1 data: " + n.activate(inputs) + ", should be " + tp.getOutput());
@@ -31,16 +43,18 @@ public class Test {
 		System.out.println("output for 2 data: " + n.activate(inputs2) + ", should be " + tp2.getOutput());
 		System.out.println("------------------------------------------------");
 		
+		// start learning until error < ERROR_VALUE
 		long iterations = 0l;
 		double errorValue = 1d;
 		double errorValue2 = 1d;
 
-		
 		while (errorValue > Constants.ERROR_VALUE || errorValue2 > Constants.ERROR_VALUE ) {
 			errorValue = Math.abs(n.backwardErrorPropagation(tp, Constants.EPSILON));
 			errorValue2 = Math.abs(n.backwardErrorPropagation(tp2, Constants.EPSILON));
 			iterations++;
 		}
+		
+		// neuron learned
 		System.out.println("needs " + iterations + " iterations to reduce error value to " 
 				+ errorValue + " and " + errorValue2);
 		System.out.println("weights: " + n.getWeights().toString());
