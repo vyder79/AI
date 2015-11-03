87f47cf3f6dac8587cceca2df28e88f4f9931884
vyder <vyder@lapek>
2015-11-03 22:16:42 +0100

    Utworzenie sieci skłądającej się z warstwy wejściowej oraz wyjściowej, bez implementacji zależności między warstwami.

--- AI_task_0/src/SingleLayer.java
+++ AI_task_0/src/SingleLayer.java
@@ -16,6 +16,15 @@ public class SingleLayer {
 		this.description = description;
 		this.singleLayer = neurons;		
 	}
+	
+	public void toStringOut(){
+		System.out.print("[");
+		for (Neuron n : this.singleLayer) {
+			n.toStringOut();
+		}
+		System.out.print("]" + this.description);
+		System.out.println("");
+	}
 
 	public ArrayList<Neuron> getSingleLayer() {
 		return singleLayer;
