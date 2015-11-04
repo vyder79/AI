dbcb267216dfee86ac4795e59d997c0ba665aaf9
vyder <vyder@lapek>
2015-11-04 23:09:10 +0100

    Dodanie normalizacji wektorów wejściowych.

--- AI_task_0/src/t.java
+++ AI_task_0/src/t.java
@@ -1,19 +1,46 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class t {
 
 	public static void main(String[] args) throws IOException {
 		ReadFile rf = new ReadFile();
 		
-//		String file = "C:/Users/Mateusz/Desktop/Literki 16x16/B.txt";
 		String file2 = "C:/Users/Mateusz/git/AI/AI_task_0/src/Letters/Alfabet.txt";
-		
-//		rf.read(file);
-//		System.out.println(rf.getInputVector().size());
-		
+		//String file2 = "C:/Users/vyder/git/AI/AI_task_0/src/Letters/Alfabet.txt";
+
 		rf.readAllLetters(file2);		
 		rf.read(rf.getAllLetters());
-		System.out.println(rf.getInputArray().get(25).size());
+		
+		/** 
+		 * przepisanie liter na wektory wejœciowe oraz normalizacja 
+		 * 
+		 * (zamiast na sztywno "26" mo¿na by jakoœ inaczej podawaæ liczbê wzorców)
+		 */
+		for(int i = 0; i < 26; i++){
+			ArrayList<Integer> ltr = new ArrayList<>(); // tablica litery zero-jedynkowa
+			ArrayList<Double> ltrNormalized = new ArrayList<>(); // tablica litery jako znormalizowane double
+			ltr.addAll(rf.getInputArray().get(i));
+			int ones = 0;
+			
+			// policzenie jedynek w literze
+			for (Integer l : ltr){
+				if (l == 1) {
+					ones++;
+				}
+			}
+			System.out.println(ltr + " jedynki:" + ones);
+			
+			// zamiana jedynek na znormalizowan¹ wartoœæ
+			for (Integer l : ltr){
+				if (l == 1) {
+					ltrNormalized.add(1/Math.sqrt(ones));
+				}
+			}
+			System.out.println(ltrNormalized + " normalized");
+		}
+		
+		
 
 
 	}
