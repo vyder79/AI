c597e50417270c6d9d48be044bb1387997cca2a0
Mati <mati.kaweczynski@gmail.com>
2015-11-04 12:40:28 +0100

    Dodanie szablonów 16x16 liter oraz pliku zawierającego wszystkie ściężki do tych szablonów, oraz klasy która czyta z pliku wszystkie informacje i przygotowuje wektory wejsciowe do sieci.

---
+++ AI_task_0/src/t.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+public class t {
+
+	public static void main(String[] args) throws IOException {
+		ReadFile rf = new ReadFile();
+		
+//		String file = "C:/Users/Mateusz/Desktop/Literki 16x16/B.txt";
+		String file2 = "C:/Users/Mateusz/git/AI/AI_task_0/src/Letters/Alfabet.txt";
+		
+//		rf.read(file);
+//		System.out.println(rf.getInputVector().size());
+		
+		rf.readAllLetters(file2);		
+		rf.read(rf.getAllLetters());
+		System.out.println(rf.getInputArray().get(0));
+
+	}
+
+}
